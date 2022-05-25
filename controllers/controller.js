6559a64f084354dd17f58e633a626cad46ca9120
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-05-25 14:58:38 -0300

    rename

--- controllers/controller.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const products = require('../services/products');
-
-const routes = express.Router();
-
-routes.get('/', async (_req, res) => {
-    const [rows] = await products.getProducts();
-    return res.status(200).json(rows);
-});
