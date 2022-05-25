abc55ef05e33ab76bd452e0b2016cf74e54640f9
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-05-25 16:25:40 -0300

    feat:(add router sales)

--- routers/index.js
+++ routers/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const productsControllers = require('../controllers/products');
+const salesControllers = require('../controllers/sales');
 
 router.use('/products', productsControllers);
+router.use('/sales', salesControllers);
+
 module.exports = router;
