ca8e1d59972453763a095c466f9ddc85248654f5
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-05-25 17:09:08 -0300

    feat:(get sales by id)

--- controllers/sales.js
+++ controllers/sales.js
@@ -9,6 +9,19 @@ routes.get('/', async (_req, res) => {
     return res.status(200).json(rows);
 });
 
+routes.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const rows = await sales.getSales(id);
+        if (rows.length === 0) {
+            res.status(404).json({ message: 'Sale not found' }); 
+        }
+        res.status(200).json(rows);
+    } catch (error) {
+        res.status(404).json({ message: 'Sale not found' }); 
+    }
+});
+
 routes.use(middlewares.errorHandler);
 
 module.exports = routes;
