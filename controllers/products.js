ab446a75a506a6b70041857790356557f9ae6a03
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-05-25 14:59:24 -0300

    feat:(endpoint get controllerss)

---
+++ controllers/products.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const products = require('../services/products');
+
+const routes = express.Router();
+
+routes.get('/', async (_req, res) => {
+    const [rows] = await products.getProducts();
+    return res.status(200).json(rows);
+});
+
+module.exports = routes;
