da16f94d5aa7b09040f3efb06870d912fc51dc17
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-05-26 00:09:02 -0300

    feat:( insert new middlewares)

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,3 +1,9 @@
 const errorHandler = require('./errorHandler');
+const validationProducts = require('./validationProducts');
+const validationSales = require('./validationSales');
 
-module.exports = { errorHandler };
+module.exports = { 
+    errorHandler, 
+    validationProducts,
+    validationSales,
+};
